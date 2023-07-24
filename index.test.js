9f024761b1f87cad6da3b21c8310cec35efabb6f
Per-Olof Bengtsson <po@niffic.se>
2023-07-24 21:06:20 +0200

    fixed faulty file names and faulty jest config

--- index.test.js
+++ index.test.js
@@ -1,3 +1,11 @@
 /**  add your tests to this file and add test files as you
  * see necessary to make your code structure readable and
  * testable */
+
+import { kalle } from "."
+
+describe('will this work', () => {
+   it('just might', () => {
+      expect(kalle()).toEqual(1)
+   })
+})
