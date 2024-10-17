48301f5b02cdb3a2f96e563d0d9b19b903f7bc85
Per-Olof Bengtsson <po@niffic.se>
2024-10-17 03:12:13 +0200

    added example code for reading input from the command line stdin

---
+++ examples/input.js
@@ -0,0 +1,27 @@
+import readline from 'node:readline'
+
+// Create readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+// Wrap `rl.question` in a promise
+function askQuestion(query) {
+  return new Promise((resolve) => {
+    rl.question(query, resolve)
+  })
+}
+
+// Using async/await to get input
+async function main() {
+  const name = await askQuestion('What is your name? ')
+  console.log(`Hello, ${name}!`)
+
+  const age = await askQuestion('How old are you? ')
+  console.log(`You are ${age} years old!`)
+
+  rl.close()
+}
+
+main()
