1941d5139f67c0be4c968c1bd45e134ef3be7a01
Per-Olof Bengtsson <po@niffic.se>
2024-10-24 07:51:08 +0200

    updated template to match new way of using docker for submitting the assignment

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# Use the official Node.js 23 image from Amazon ECR Public Gallery
+FROM public.ecr.aws/docker/library/node:23
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y git
+
+RUN git clone https://github.com/pobengtsson/bth012-a1-part-a.git .
+
+ENTRYPOINT ["node", "src/index.js"]
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
  * as necessary to make your code structure more testable and
  * easy to understand */
 
-export function kalle () {
-  return 1
-}
+import askQuestion from "./askQuestion"
+
+const answer = askQuestion('Can you see this message? Then type "yes" and press Enter.')
+console.log("You entered: ", answer)
